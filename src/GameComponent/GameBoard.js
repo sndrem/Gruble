ca37cba2584cc9f7af65146361f05a662604d736
Sindre <sndrem@live.no>
2018-01-08 16:15:16 +0100

    Trying to work out the best way to check for accepting letters

--- src/GameComponent/GameBoard.js
+++ src/GameComponent/GameBoard.js
@@ -8,23 +8,41 @@ class GameBoard extends Component {
 	}
 
 	createTableRows(rows) {
-		return rows.map((row, i) => <th key={i}>{row}</th>);
+		return rows.map((row, i) => <th className="keys" key={i}>{row}</th>);
 	}
 
-	fillData(nums) {
+	keyPressed(e) {
+		const letter = e.target.dataset.letter;
+		const value = e.target.value.toLowerCase();
+
+		if(letter !== value && value !== '') {
+			e.target.classList.add("error");
+		} else {
+			e.target.classList.remove("error");
+		}
+		
+	}
+
+	fillData(nums, letter) {
 		let elements = [];
 		for(let i = 0; i < nums; i++) {
-			elements.push(<td key={i}></td>)
+			elements.push(
+					<td className="user-generated-content" key={i}>
+						<div className="flex-container">
+							<input data-letter={letter.toLowerCase()} onKeyUp={this.keyPressed} type="text"/> 
+							<span className="score"></span>
+						</div>
+					</td>
+				)
 		}
 		return elements;
 	}
 
 	createTableColumns(columns) {
-
 		return columns.map((col, i) => {
 			return <tr key={i}>
-					<td key={i}>{col}</td>
-					{this.fillData(this.state.columns.length)}
+					<td className="keys" key={i}>{col}</td>
+					{this.fillData(this.state.columns.length, col)}
 				</tr>
 		})
 	}
@@ -35,7 +53,7 @@ class GameBoard extends Component {
 					<table>
 						<thead>
 							<tr>
-							<th></th>
+							<th className="hidden"></th>
 								{ this.createTableRows(this.state.rows)  }
 							</tr>
 						</thead>
